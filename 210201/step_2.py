8e49ed64db631da5eb57b685c20db50c06f6ecde
Hp <lolitakur72@gmail.com>
2021-02-07 20:21:27 +0500

    1

--- 210201/step_2.py
+++ 210201/step_2.py
@@ -1,5 +1,18 @@
-my_var_1 = [0, False, '', []]
-my_var_2 = [0, False, '5', []]
-my_var_3 = [1, True, '5', [0]]
-print(any(my_var_1), any(my_var_2), any(my_var_3))
-print(all(my_var_1), all(my_var_2), all(my_var_3))
+import re
+
+import requests
+
+# RE_XLS_FILE = re.compile(r'<a.+</a>')
+# RE_XLS_FILE = re.compile(r'href=".+"')
+RE_XLS_FILE = re.compile(r'href="[^"]+"')
+
+request_url = 'https://kpk.kss45.ru/%D1%83%D1%87%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0/%D1%80%D0%B0%D1%81%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BF%D0%B0%D1%80.html'
+
+response = requests.get(request_url)
+content = response.content.decode(encoding=response.encoding)
+
+# "https://kpk.kss45.ru/attachments/article/2655/Расписание на 23-28 ноября по преподавателям А-Л2.xls"
+parsed = RE_XLS_FILE.findall(content)
+parsed = filter(lambda x: x.endswith('.xls"'), parsed)
+# print(len(parsed))
+print(*parsed, sep='\n\n')
