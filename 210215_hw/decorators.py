c928648b1cc742083154a49fa40633b694e9b1c7
Hp <lolitakur72@gmail.com>
2021-02-21 19:16:34 +0500

    5

--- 210215_hw/decorators.py
+++ 210215_hw/decorators.py
@@ -1,5 +1,11 @@
+# def do_twice(func):
+#     def wrapper_do_twice():
+#         func()
+#         func()
+#     return wrapper_do_twice
+
 def do_twice(func):
-    def wrapper_do_twice():
-        func()
-        func()
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
     return wrapper_do_twice
